46610206bc6ea534da25237baa003d2470fd0052
CodingNossel <lpnossel@gmail.com>
2024-09-22 01:11:14 +0200

    added intersection test [revert from the revert]

--- rnmdplc.cpp
+++ rnmdplc.cpp
@@ -17,7 +17,7 @@ typedef CGAL::Convex_hull_traits_adapter_2<K,
     CGAL::Pointer_property_map<Point_2>::type > Convex_hull_traits_2;
 
 
-Rect_2 do_rmd_obj()
+Rect_2 doRmdObj()
 {
     int top_xrand = std::experimental::randint(1, 5);
     int top_yrand = std::experimental::randint(1, 5);
@@ -26,7 +26,7 @@ Rect_2 do_rmd_obj()
     return rect;
 }
 
-Circle_2 get_circle() {
+Circle_2 getCircle() {
     const double square = 10.0;
     Circle_2 circle = Circle_2(Point_2(0,0), square);
 
@@ -37,22 +37,19 @@ Circle_2 get_circle() {
 }
 
 
-std::vector<Rect_2> get_obj(std::vector<Rect_2> in ,int n)
+std::vector<Rect_2> getObj(std::vector<Rect_2> in ,int n)
 {
     for (int i = 0; i < n; ++i)
     {
-        in.push_back(do_rmd_obj());
+        in.push_back(doRmdObj());
     }
     return in;
 }
 
-Point_2 get_rnmd_loc(){
-    return Point_2(std::experimental::randint(0,30), std::experimental::randint(0,30));
+Point_2 getRmdLoc(int xmin, int xmax, int ymin, int ymax){
+    return Point_2(std::experimental::randint(xmin, xmax), std::experimental::randint(ymin, ymax));
 }
 
-bool isIntersected(Circle_2 placable_circle, std::vector<Rect_2> rects, std::vector<Circle_2> circles) {
-    return false;
-}
 
 std::vector<Rect_2> adjustPosition(std::vector<Rect_2> rect_list) {
 
@@ -80,60 +77,61 @@ std::vector<Rect_2> adjustPosition(std::vector<Rect_2> rect_list) {
     return rect_list;
 }
 
+Circle_2 getRmdLocOnMapCrcl(Circle_2 crcl, const double square) {
+    Point_2 loc = getRmdLoc(0, 30, 0, 10);
+    return Circle_2(loc, square);
+}
+
+
+bool isIntersected(Circle_2 placable_circle, std::vector<Rect_2> rects, std::vector<Circle_2> circles) {
+    for (Rect_2 rect : rects) {
+        if (CGAL::do_intersect(placable_circle, rect)) return true;
+    
+    }
+    std::cout << "No intersection with Rects" << std::endl;
+    std::cout << "Circle Pos:" << placable_circle.bbox() << ", square:" << placable_circle.squared_radius() << std::endl;
+    return false;
+
+}
 
 
 int main()
 {
-    //Idea: Größe der Map, und dann Rects nochmal umändern von der def mit der größe der map und dann hinzufügen
-    // Problem find a multidimensional datastructure which accept more than one datastructre
-    // CGAL::Pointer_property_map<Point_2> prop_map {}; //do something there
-
     std::vector<Rect_2> outer_section_map = { Rect_2(Point_2(0,0), Point_2(-1, 10)),
                                           Rect_2(Point_2(0,10), Point_2(30, 11)),
                                           Rect_2(Point_2(30,0), Point_2(31, 10)),
                                           Rect_2(Point_2(0,0), Point_2(30, -1)) };
 
-    std::vector<Point_2> map = { Point_2(0,0),
-                                 Point_2(30,0),
-                                 Point_2(30,10),
-                                 Point_2(0,10) };
 
-
-
-    std::vector<Rect_2> placable_obj {get_obj({}, 5)};
-
-
-    // for (size_t i = 0; i < placable_obj.size(); ++i)
-    // {
-
-    //     std::cout << "obj[" << i << "] = " << placable_obj[i].size() << std::endl;
-    //     std::vector<size_t> indices(placable_obj[i].size()), out;
-    //     std::iota(indices.begin(), indices.end(), 0);
-    //     CGAL::convex_hull_2(indices.begin(), indices.end(), std::back_inserter(out),
-    //                         Convex_hull_traits_2(CGAL::make_property_map(placable_obj[i])));
-    //     for (size_t p : out)
-    //     {
-    //         std::cout << "points[" << p << "] = " << placable_obj[i][p] << std::endl;
-    //     }
-    // }
-
-    get_circle();
-    std::vector<Rect_2> vec_re {};
-
-    vec_re = get_obj(vec_re,4);
-    std::cout << vec_re.size() << std::endl;
-    for (auto& i : vec_re) {
-        CGAL::Bbox_2 bbox = i.bbox();
-        std::cout << "Rect Box: " << bbox << std::endl;
-    }
-
-    std::vector<Rect_2> rect_list = get_obj(rect_list, 4);
+    std::vector<Rect_2> rect_list = getObj(rect_list, 4);
     for (auto& rect : rect_list) std::cout << rect << std::endl;
     
     rect_list = adjustPosition(rect_list);
     for (auto& rect : rect_list) std::cout << rect << std::endl;
 
+    rect_list.insert(rect_list.end(), outer_section_map.begin(), outer_section_map.end());
+
+    std::cout << rect_list.size() << std::endl;
+    Circle_2 place_obj_crcl= getCircle();
+
+    double square = 10.0;    
     
+    bool cond_Var = true;
+    int mk_crcl_smaller = 0;
+
+    while (cond_Var) {
+
+        if (mk_crcl_smaller%10)
+        {
+            square -= 1.0;
+        }
+        
+        place_obj_crcl = getRmdLocOnMapCrcl(place_obj_crcl, square);
+        cond_Var = isIntersected(place_obj_crcl, rect_list, {});
+        mk_crcl_smaller++;
+    }
+    
+
     return 0;
 }
 
